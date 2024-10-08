292c7710446bdfe1354e94c9e59e3ee293435876
suhail-kp00 <shalusalmaan44@gmail.com>
2024-10-08 16:27:54 +0530

    update

--- form.py
+++ form.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 app = Flask(__name__)
-client=MongoClient('mongodb://localhost:27017/')
+client=MongoClient('mongodb+srv://suhail:suhail123@cluster0.pxc97.mongodb.net/')
 db=client['flask_db']
 collection=db['submissions']
 @app.route('/', methods=['GET', 'POST'])
